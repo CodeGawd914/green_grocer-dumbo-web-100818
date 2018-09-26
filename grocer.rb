dc14fa181105600df34ee7e66e21e44bc1a54dfb
Travis Desir <travisgdesir31@gmail.com>
2018-09-26 16:38:03 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -5,9 +5,9 @@ def consolidate_cart(cart)
     item = food.keys[0]
     haxh[item] = food.values[0]
     if haxh[item][:count]
-      haxh[item][:count] = 1 
-    else
       haxh[item][:count] += 1 
+    else
+      haxh[item][:count] = 1 
     end
   end
   haxh
