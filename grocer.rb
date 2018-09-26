3062cd85626f8c437daac185404afe6b96b4217a
Travis Desir <travisgdesir31@gmail.com>
2018-09-26 17:43:33 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -45,6 +45,22 @@ def apply_clearance(cart)
   end
 end
 
-def checkout(cart:[], coupons:[])
-  
+def checkout(cart: [], coupons: [])
+	 cart = consolidate_cart(cart: cart)
+end 	
+  cart = apply_coupons(cart: cart, coupons: coupons)
+  cart = apply_clearance(cart: cart)
+  total = 0
+  cart.each do |item_name, item_data|
+    total += (item_data[:price] * item_data[:count])
+  end
+   if total > 100
+    0.9 * total
+  else
+    total
+  end
 end
+
+# def checkout(cart:[], coupons:[])
+  
+# end
